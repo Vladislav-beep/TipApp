3efec3d332b1d72008471f949f30bd54f9baa8f1
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-08-31 22:30:55 +0300

    set up buttons

--- TipApp/ViewController.swift
+++ TipApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billTextField: UITextField!
+    
+    @IBOutlet var peopleCountLabel: UILabel!
+    
+    @IBOutlet var zeroButton: UIButton!
+    @IBOutlet var tenButton: UIButton!
+    @IBOutlet var fifteenButton: UIButton!
+    @IBOutlet var resultButton: UIButton!
+    
+    var currentTip = 0.0
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    
+    
+    @IBAction func buttonsPressed(_ sender: UIButton) {
+        zeroButton.isSelected = false
+        tenButton.isSelected = false
+        fifteenButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = String(sender.currentTitle!)
+        let tip = Double(buttonTitle.dropLast())! / 100
+        currentTip = tip
+    }
 
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        peopleCountLabel.text = String(format: "%.0f",sender.value)
+    }
 }
 
